2147ad2dc426d27109ecbbfa349603c71bed3410
harshitsingh-14 <harshit.singh@iiitg.ac.in>
2023-07-27 16:33:18 +0800

    added sdxl

---
+++ examples/dreambooth/run_lora_sdxl.sh
@@ -0,0 +1,39 @@
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-0.9"
+export INSTANCE_DIR="/home/rtx4090/HSN/dataset/backpack"
+export OUTPUT_DIR="/home/rtx4090/HSN/outputs"
+
+accelerate launch train_dreambooth_lora_sdxl.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --mixed_precision="fp16" \
+  --instance_prompt="a photo of aks backpack" \
+  --resolution=1024 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=10 \
+  --adapter_type="krona" \
+  --lora_rank=4 \
+  --seed="0"
+
+python3 generator.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --mixed_precision="fp16" \
+  --instance_prompt="a photo of aks backpack" \
+  --resolution=1024 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --learning_rate=1e-4 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=10 \
+  --adapter_type="krona" \
+  --lora_rank=4 \
+  --seed="0"
+
+
