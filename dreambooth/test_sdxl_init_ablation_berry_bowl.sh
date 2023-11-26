3009f501668a32f7482aefbfcac24a9862500aa7
shyammarjit <shyam.marjit@iiitg.ac.in>
2023-11-26 11:20:39 +0000

    updated init ablation

--- dreambooth/test_sdxl_init_ablation_berry_bowl.sh
+++ dreambooth/test_sdxl_init_ablation_berry_bowl.sh
@@ -17,7 +17,7 @@ krona_unet_o_rank_a1=$a1 # out
 krona_unet_o_rank_a2=$a2 # out
 
 lr=6e-4
-steps=2
+steps=1000
 
 
 accelerate launch train_dreambooth_lora_sdxl.py \
@@ -50,31 +50,32 @@ accelerate launch train_dreambooth_lora_sdxl.py \
     --krona_unet_o_rank_a2=$krona_unet_o_rank_a2 \
     --init_type="type_2" 
 
-# python3 generator.py \
-#     --pretrained_model_name_or_path=$MODEL_NAME \
-#     --instance_data_dir=$INSTANCE_DIR \
-#     --output_dir=$OUTPUT_DIR \
-#     --mixed_precision="fp16" \
-#     --instance_prompt="a photo of sks${subjects}" \
-#     --resolution=1024 \
-#     --train_batch_size=1 \
-#     --gradient_accumulation_steps=4 \
-#     --learning_rate=$lr \
-#     --lr_scheduler="constant" \
-#     --lr_warmup_steps=0 \
-#     --max_train_steps=$steps \
-#     --adapter_type="krona" \
-#     --seed="0" \
-#     --diffusion_model="sdxl" \
-#     --use_8bit_adam \
-#     --gradient_checkpointing \
-#     --enable_xformers_memory_efficient_attention \
-#     --attn_update_unet=$attn_update_unet \
-#     --krona_unet_k_rank_a1=$krona_unet_k_rank_a1 \
-#     --krona_unet_k_rank_a2=$krona_unet_k_rank_a2 \
-#     --krona_unet_q_rank_a1=$krona_unet_q_rank_a1 \
-#     --krona_unet_q_rank_a2=$krona_unet_q_rank_a2 \
-#     --krona_unet_v_rank_a1=$krona_unet_v_rank_a1 \
-#     --krona_unet_v_rank_a2=$krona_unet_v_rank_a2 \
-#     --krona_unet_o_rank_a1=$krona_unet_o_rank_a1 \
-#     --krona_unet_o_rank_a2=$krona_unet_o_rank_a2 \
+python3 generator.py \
+    --pretrained_model_name_or_path=$MODEL_NAME \
+    --instance_data_dir=$INSTANCE_DIR \
+    --output_dir=$OUTPUT_DIR \
+    --mixed_precision="fp16" \
+    --instance_prompt="a photo of sks${subjects}" \
+    --resolution=1024 \
+    --train_batch_size=1 \
+    --gradient_accumulation_steps=4 \
+    --learning_rate=$lr \
+    --lr_scheduler="constant" \
+    --lr_warmup_steps=0 \
+    --max_train_steps=$steps \
+    --adapter_type="krona" \
+    --seed="0" \
+    --diffusion_model="sdxl" \
+    --use_8bit_adam \
+    --gradient_checkpointing \
+    --enable_xformers_memory_efficient_attention \
+    --attn_update_unet=$attn_update_unet \
+    --krona_unet_k_rank_a1=$krona_unet_k_rank_a1 \
+    --krona_unet_k_rank_a2=$krona_unet_k_rank_a2 \
+    --krona_unet_q_rank_a1=$krona_unet_q_rank_a1 \
+    --krona_unet_q_rank_a2=$krona_unet_q_rank_a2 \
+    --krona_unet_v_rank_a1=$krona_unet_v_rank_a1 \
+    --krona_unet_v_rank_a2=$krona_unet_v_rank_a2 \
+    --krona_unet_o_rank_a1=$krona_unet_o_rank_a1 \
+    --krona_unet_o_rank_a2=$krona_unet_o_rank_a2 \
+    --init_type="type_2" 
